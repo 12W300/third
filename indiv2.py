d8a56cd40d110306c70e5da53e7202ba70a2dbae
12W300 <78951373+12W300@users.noreply.github.com>
2021-06-04 06:53:07 +0300

    Add files via upload

---
+++ indiv2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+if __name__ == '__main__' :
+
+    a = int(input())
+    b = int(input())
+    c = int(input())
+
+    m = a
+    if m < b:
+        m = b
+    if m < c:
+        m = c
+
+    print(m)
+
+    n = a
+    if n > b:
+        n = b
+    if n > c:
+        n = c
+
+    print(n)
